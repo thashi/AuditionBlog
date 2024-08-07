1305189e29e6abf0ccfda636a1ad55e5e1b81ee6
Thashitharan Rajaratnam <thashitharan.r@gmail.com>
2024-08-07 19:52:19 +1000

    test changes

--- src/test/java/com/audition/AuditionControllerTests.java
+++ src/test/java/com/audition/AuditionControllerTests.java
@@ -1,10 +1,13 @@
 package com.audition;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -17,31 +20,46 @@ class AuditionControllerTests {
 
     @Test
     void testGetPosts() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/posts"))
-            .andExpect(MockMvcResultMatchers.status().isOk());
+        final MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/posts"))
+            .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+
+        final String content = result.getResponse().getContentAsString();
+        assertNotNull(content);
     }
 
     @Test
     void testGetPostsWithParam() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/posts?userId=1"))
-            .andExpect(MockMvcResultMatchers.status().isOk());
+        final MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/posts?userId=1"))
+            .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+
+        final String content = result.getResponse().getContentAsString();
+        assertNotNull(content);
     }
 
     @Test
     void testGetPostsWithId() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/posts/1"))
-            .andExpect(MockMvcResultMatchers.status().isOk());
+        final MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/posts/1"))
+            .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+
+        final String content = result.getResponse().getContentAsString();
+        assertNotNull(content);
     }
 
     @Test
     void testGetPostWithComments() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/posts/1/comments"))
-            .andExpect(MockMvcResultMatchers.status().isOk());
+        final MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/posts/1/comments"))
+            .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+
+        final String content = result.getResponse().getContentAsString();
+        assertNotNull(content);
     }
 
     @Test
     void testGetComments() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/comments?postId=1"))
-            .andExpect(MockMvcResultMatchers.status().isOk());
+        final MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/comments?postId=1"))
+            .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+
+        final String content = result.getResponse().getContentAsString();
+        assertNotNull(content);
     }
 }
